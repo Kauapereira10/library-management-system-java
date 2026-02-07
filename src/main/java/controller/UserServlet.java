b9f13cc4bf70413624a4f34f4509680a5070bf93
Kaua Pereira <borgeskaua1210@gmail.com>
2026-02-07 03:03:57 -0300

    feat(controller): cria servlet base para ações de usuário
    Adiciona UserServlet mapeado para /user, com estrutura inicial para tratar requisições GET e POST relacionadas a cadastro e login.

--- src/main/java/controller/UserServlet.java
+++ src/main/java/controller/UserServlet.java
@@ -1,5 +1,34 @@
 package controller;
 
-public class UserServlet {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/user")
+public class UserServlet extends HttpServlet{	
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getServletPath();
+		
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getServletPath();
+		
+		switch (action) {
+		case "": {
+			
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value");
+		}
+	}
+	
 }
