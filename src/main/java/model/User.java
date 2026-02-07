37ef53ebc13edfa93ba65dbd0fd7dae5e23ded75
Kaua Pereira <borgeskaua1210@gmail.com>
2026-02-07 03:05:41 -0300

    feat(model): adiciona classe User para cadastro e autenticação
    Cria model User com id, nome completo, nickname, email e passwordHash, incluindo construtores, getters e setters, preparado para login e registro.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,70 @@
 package model;
 
 public class User {
+	private Long id;
+	private String fullName;
+	private String nickName;
+	private String email;
+	private String passwordHash;
+	
+	public User() {
+	
+	}
+	
+	public User(String fullName, String nickName, String email, String passwordHash) {
+		this.fullName = fullName;
+		this.nickName = nickName;
+		this.email = email;
+		this.passwordHash = passwordHash;
+	}
 
+	public User(Long id, String fullName, String nickName, String email, String passwordHash) {
+		super();
+		this.id = id;
+		this.fullName = fullName;
+		this.nickName = nickName;
+		this.email = email;
+		this.passwordHash = passwordHash;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}	
+	
 }
