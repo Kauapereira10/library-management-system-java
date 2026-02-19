1214bb8560c489a07c7db4198b33a5eb2d5e943f
Kauã Pereira <borgeskaua1210@gmail.com>
2026-02-18 21:07:56 -0300

    feat: implementa listagem de livros no BookServlet
    Adiciona instância de BookDAO.
    Cria método listBooks().
    Implementa roteamento no doGet para tratar a rota /books.
    Integra servlet com a view list.jsp.

--- src/main/java/controller/BookServlet.java
+++ src/main/java/controller/BookServlet.java
@@ -1,38 +1,55 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.BookDAO;
+import model.Book;
+
+
+
 
 @WebServlet("/books")
 public class BookServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BookServlet() {
-        super();
-        
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	private BookDAO bookDao = null;
+
+	public BookServlet() {
+		bookDao = new BookDAO();
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Rota acessada: " + request.getServletPath());
 		
-		request.getRequestDispatcher("WEB-INF/views/books/list.jsp").forward(request, response);
+		String action = request.getServletPath();
+		
+		switch (action) {
+		case "/books": 
+			listBooks(request, response);
+			break;
+		default:
+			request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
+		}
 		
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
+	private void listBooks(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		List<Book> books = bookDao.findAll();
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/books/list.jsp");
+		dispatcher.forward(request, response);
+	}
+	
 }
