a1aa1c03241fbcf27fe7366c75451183fbbfafaa
Kauã Pereira <borgeskaua1210@gmail.com>
2026-02-18 21:08:32 -0300

    feat: cria BookDetailsServlet para exibir detalhes do livro
    Adiciona servlet mapeado em /books/details.
    Instancia BookDAO para busca de dados.
    Cria método detailsBook().
    Implementa estrutura de roteamento no doGet().
    Prepara integração com a view book-details.jsp.

--- src/main/java/controller/BookDetailsServlet.java
+++ src/main/java/controller/BookDetailsServlet.java
@@ -1,26 +1,48 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.BookDAO;
+import model.Book;
+
 
 @WebServlet("/books/details")
 public class BookDetailsServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	
+	private BookDAO bookDao = null;
        
     public BookDetailsServlet() {
-        super();
-
+    	bookDao = new BookDAO();
     }
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Rota acessada: " + request.getServletPath());
+		
+		String action = request.getServletPath();
+		
+		switch (action) {
+		case "/books/details": 
+			//detailsBook(request, response);
+			break;
+		default:
+			request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
+		}
+	}
+	
+	private void detailsBook (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		List<Book> book = bookDao.findById();
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/books/book-details.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
