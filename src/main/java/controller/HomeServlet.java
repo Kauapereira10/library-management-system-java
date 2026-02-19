07facc0113731e7bedca98678e7a6a2fefaf2307
Kauã Pereira <borgeskaua1210@gmail.com>
2026-02-18 21:06:17 -0300

    refactor: altera mapeamento do HomeServlet de /Home para /home para padronização de URL

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/Home")
+@WebServlet("/home")
 public class HomeServlet extends HttpServlet {
        
 	@Override
